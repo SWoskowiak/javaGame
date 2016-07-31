63ab4934043d897465a84e22a8d906a13ea8cc21
swoskowiak <swoskowiak@gmail.com>
2016-07-31 14:46:31 -0400

    added ship

--- test/core/src/com/mygdx/game/Ship.java
+++ test/core/src/com/mygdx/game/Ship.java
@@ -8,14 +8,17 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
+import static com.badlogic.gdx.Gdx.input;
+
 /**
  * Created by stefan on 7/30/16.
  */
 public class Ship extends Actor{
     // Our ship texture
     private Texture img;
+    private float speed = 250;
 
-    public void Ship() {
+    public Ship() {
         // Our ship's texture
         img = new Texture("ship.png");
 
@@ -23,11 +26,39 @@ public class Ship extends Actor{
         this.setBounds(0, 0, img.getWidth(), img.getHeight());
     }
 
+    private void checkMovement(float Delta) {
+        // Move Left
+        if(input.isKeyPressed(Input.Keys.A) || input.isKeyPressed(Input.Keys.LEFT)) {
+            this.setX(this.getX() - (speed * Delta));
+        }
+        // Move Right
+        if (input.isKeyPressed(Input.Keys.D) || input.isKeyPressed(Input.Keys.RIGHT)) {
+            this.setX(this.getX() + (speed * Delta));
+        }
+        // Move Up
+        if (input.isKeyPressed(Input.Keys.W) || input.isKeyPressed(Input.Keys.UP)) {
+            this.setY(this.getY() + (speed * Delta));
+        }
+        // Move Down
+        if (input.isKeyPressed(Input.Keys.S) || input.isKeyPressed(Input.Keys.DOWN)) {
+            this.setY(this.getY() - (speed * Delta));
+        }
+    }
+
+    private void checkFiring(float Delta) {
+        if (input.isKeyPressed(Input.Keys.SPACE)) {
+            this.setY(this.getY() - (speed * Delta));
+        }
+    }
+
     @Override
     public void act(float Delta) {
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-           this.setX(this.getX() - (5 * Delta));
-           System.out.println("x: " + this.getX());
-        }
+        checkMovement(Delta);
+        checkFiring(Delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(img, this.getX(), this.getY());
     }
 }
